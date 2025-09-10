02d27bf71d58d54bfb8c34041d3b4c027fb24e7a
Trivo-AmrMohammed <165791008+Trivo-AmrMohammed@users.noreply.github.com>
2025-09-10 11:07:31 +0300

    pretty much done, some tweaking maybe but no need

--- src/main/java/net/trivo/mod/TrivoMod.java
+++ src/main/java/net/trivo/mod/TrivoMod.java
@@ -1,75 +1,32 @@
 package net.trivo.mod;
 
-import net.minecraft.tags.ItemTags;
-import net.minecraft.util.profiling.jfr.event.ChunkGenerationEvent;
-import net.minecraft.world.item.*;
-import net.minecraft.world.level.storage.loot.LootParams;
-import net.minecraft.world.level.storage.loot.entries.LootItem;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParam;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
-import net.neoforged.neoforge.event.LootTableLoadEvent;
-import net.neoforged.neoforge.event.tick.PlayerTickEvent;
-import net.trivo.mod.Config;
-import org.slf4j.Logger;
-
-import com.mojang.logging.LogUtils;
-
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.item.*;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
-import java.rmi.registry.Registry;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Random;
+import java.util.function.Supplier;
 
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(net.trivo.mod.TrivoMod.MODID)
 public class TrivoMod {
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "whatnetherite";
-    // Directly reference a slf4j logger
-    public static final Logger LOGGER = LogUtils.getLogger();
+    public static List<ItemStack> combatItemList = new ArrayList<>();
 
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public TrivoMod(IEventBus modEventBus, ModContainer modContainer) {
-        // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
-
-        // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
-        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
+    public TrivoMod(IEventBus modEventBus) {
         NeoForge.EVENT_BUS.register(this);
-
-        // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
-
-        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
-        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+        CREATIVE_MODE_TABS.register(modEventBus);
     }
 
-    private void commonSetup(FMLCommonSetupEvent event) {
-     }
-
-    // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
         for (ItemStack stack : event.getSearchEntries()) {
             if (stack.getItem().toString().contains("netherite")) {
@@ -78,32 +35,43 @@ public class TrivoMod {
             if (stack.getItem().equals(Items.ANCIENT_DEBRIS)) {
                 event.remove(stack, CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
             }
+            if (event.getTabKey() == CreativeModeTabs.COMBAT && combatItemList.isEmpty()) {
+                combatItemList.addAll(event.getSearchEntries());
+                for (ItemStack item : combatItemList) {
+                    event.remove(item, CreativeModeTab.TabVisibility.PARENT_TAB_ONLY);
+                }
+            }
         }
     }
 
     @SubscribeEvent
     public void onCommand(net.neoforged.neoforge.event.CommandEvent event) {
-        Random random = new Random();
+        int random = new Random().nextInt(3);
         String input = event.getParseResults().getReader().getString();
-        if (input.contains("minecraft:netherite")) {
+        if (input.contains("minecraft:netherite") || input.contains("minecraft:ancient_debris")) {
             event.setCanceled(true);
-            if (random.nextInt(2) == 1) {
+            if (random == 1) {
                 event.getParseResults().getContext().getSource().sendFailure(
-                        net.minecraft.network.chat.Component.literal("what netherite?"));
-            } else {
+                        net.minecraft.network.chat.Component.literal("What netherite?"));
+            } else if (random == 2){
                 event.getParseResults().getContext().getSource().sendFailure(
                         net.minecraft.network.chat.Component.literal("I don't even think that's a real item."));
+            } else {
+                event.getParseResults().getContext().getSource().sendFailure(
+                        net.minecraft.network.chat.Component.literal("Huh?"));
             }
         }
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-    }
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
-    //remove netherite and template from bastion loot tables
-    //change combat tab icon from netherite sword to mace
-    //simplify the codespace to the fullest
-    //anything else needed
+    public static final Supplier<CreativeModeTab> COMBAT_TAB = CREATIVE_MODE_TABS.register("combat",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(Items.MACE))
+                    .title(Component.translatable("itemGroup.combat"))
+                    .displayItems((params, output) -> {
+                        combatItemList.forEach(output::accept);
+                    })
+                    .withTabsBefore(CreativeModeTabs.COMBAT.location())
+                    .build());
 }
