6432c4ad948ad6e26717afdc5386cf6b4ee14a35
Trivo-AmrMohammed <165791008+Trivo-AmrMohammed@users.noreply.github.com>
2025-09-10 12:27:06 +0300

    starting totem removal

--- src/main/java/net/trivo/totem/TrivoMod.java
+++ src/main/java/net/trivo/totem/TrivoMod.java
@@ -1,16 +1,19 @@
-package net.trivo.mod;
+package net.trivo.totem;
 
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Items;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 
-@Mod(net.trivo.mod.TrivoMod.MODID)
+@Mod(net.trivo.totem.TrivoMod.MODID)
 public class TrivoMod {
-    public static final String MODID = "trivomod";
+    public static final String MODID = "whattotem";
     public TrivoMod(IEventBus modEventBus) {
         modEventBus.addListener(this::addCreative);
     }
 
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        event.remove(Items.TOTEM_OF_UNDYING.getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
     }
 }
