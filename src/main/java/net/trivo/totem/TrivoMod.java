c3a66df039ebf44c0ea4fe3d15bd26001ed474ed
Trivo-AmrMohammed <165791008+Trivo-AmrMohammed@users.noreply.github.com>
2025-09-10 12:41:07 +0300

    starting totem removal

--- src/main/java/net/trivo/totem/TrivoMod.java
+++ src/main/java/net/trivo/totem/TrivoMod.java
@@ -3,17 +3,42 @@ package net.trivo.totem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Items;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.CommandEvent;
+
+import java.util.Random;
 
 @Mod(net.trivo.totem.TrivoMod.MODID)
 public class TrivoMod {
     public static final String MODID = "whattotem";
     public TrivoMod(IEventBus modEventBus) {
+        NeoForge.EVENT_BUS.register(this);
         modEventBus.addListener(this::addCreative);
     }
 
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
         event.remove(Items.TOTEM_OF_UNDYING.getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+    }
+
+    @SubscribeEvent
+    public void onCommand(CommandEvent event) {
+        int random = new Random().nextInt(3);
+        String input = event.getParseResults().getReader().getString();
+        if (input.contains("minecraft:totem_of_undying")) {
+            event.setCanceled(true);
+            if (random == 1) {
+                event.getParseResults().getContext().getSource().sendFailure(
+                        net.minecraft.network.chat.Component.literal("What totem?"));
+            } else if (random == 2){
+                event.getParseResults().getContext().getSource().sendFailure(
+                        net.minecraft.network.chat.Component.literal("I don't even think that's a real item."));
+            } else {
+                event.getParseResults().getContext().getSource().sendFailure(
+                        net.minecraft.network.chat.Component.literal("Huh?"));
+            }
+        }
     }
 }
